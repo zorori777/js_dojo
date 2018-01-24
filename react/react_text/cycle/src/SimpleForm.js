0d20a9e98a5295f53262759886898cd68cc888f2
zorori777 <e2fde7ded7anxbne05se@yahoo.co.jp>
2018-01-24 18:54:40 +0900

    react 学習4

--- react/react_text/cycle/src/SimpleForm.js
+++ react/react_text/cycle/src/SimpleForm.js
@@ -1,31 +1,54 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-export class SimpleForm extends React.Component {
+export default class SimpleForm extends Component {
   constructor (props) {
     super(props)
-    this.state = { value: '' }
+    this.state = {
+      name: 'クジラ',
+      age: 22,
+      hobby: '読書'
+    }
   }
 
   doChange (e) {
-    const newValue = e.target.value
-    this.setState({value: newValue})
+    const userValue = e.target.value
+    const key = e.target.name
+    this.setState({[key]: userValue})
   }
 
   doSubmit (e) {
-    window.alert('値を送信: ' + this.state.value)
     e.preventDefault()
+    const j = JSON.stringify(this.state)
+    window.alert(j)
   }
 
   render () {
     const doSubmit = (e) => this.doSubmit(e)
     const doChange = (e) => this.doChange(e)
-
     return (
       <form onSubmit={doSubmit}>
-        <input type='text'
-          value={this.state.value}
+        <div><label>
+          名前: <br />
+          <input name='name'
+            type='text'
+            value={this.state.name}
+            onChange={doChange} />
+        </label></div>
+        <div><label>
+          年齢: <br />
+          <input name='age'
+          type='number'
+          value={this.state.age}
           onChange={doChange} />
-          <input type='submit' value='送信'　/>
+        </label></div>
+        <div><label>
+          趣味: <br />
+          <input name='hobby'
+          type='number'
+          value={this.state.hobby}
+          onChange={doChange} />
+        </label></div>
+        <input type='submit' value='送信' />
       </form>
     )
   }
